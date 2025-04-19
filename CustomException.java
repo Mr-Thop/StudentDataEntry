c26c4553bfcb2e892dd1c2f7f2c4d48592e0c575
Karthik Reddy <89624360+Mr-Thop@users.noreply.github.com>
2025-04-19 14:13:12 +0530

    Creating CustomException.java

---
+++ CustomException.java
@@ -0,0 +1,8 @@
+// CustomException.java
+import java.lang.Exception
+
+public class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
